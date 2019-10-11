da84354dc4791aa1371a796c4fa6f8592e668ec0
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-11 15:05:50 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -3,4 +3,12 @@ const server = require('./lib/server');
 
 config.PORT = process.env.PORT || config.PORT;
 
+
+const url = window.location.search;
+url = url.replace("?varName=", ''); // remove the ?
+console.log('lllllllllllll',url);
+    const abc = url;
+   console.log('kkkkkkkkkkkkkk',abc);
+
+
 server.run(config);
