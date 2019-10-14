43ed57cd55b32ce762433d6be6b99f4f04acd318
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-14 16:55:18 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -46,7 +46,7 @@ class MainWindow extends Component {
             className="txt-clientId"
             spellCheck={false}
             placeholder="Your friend ID"
-           value="c"}
+           value="c"
           />
            
           <div>
