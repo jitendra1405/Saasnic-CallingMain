b8693a45220f406a0065e58378de1450a5e4a7c2
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-15 15:07:15 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -51,7 +51,7 @@ class MainWindow extends Component {
             className="txt-clientId"
             spellCheck={false}
             placeholder="Your friend ID"
-           defaultValue={abc}
+           onChange={event => friendID = event.target.value}
           />
           
           <div>
