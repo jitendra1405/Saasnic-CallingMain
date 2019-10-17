9c78ca8164ca5a4114951571fbda7b07e72e1aa8
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-17 15:53:15 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -9,14 +9,72 @@ var bcd = searchParams.get('varName');
 console.log('loooooooooooooooooooooooooo',abc);
 console.log('loooooooooooooooooooooooooo',bcd);
 
-
-  window.onload = function(){
-    console.log('5555555555555555555555555555555555',bcd);
+class MainWindow extends Component {
+  /**
+   * Start the call with or without video
+   * @param {Boolean} video
+   */
+  callWithVideo(video) {
     const { startCall } = this.props;
     const config = { audio: true, video};
-    startCall(true, bcd, config);
+    return () => startCall(true, friendID, config);
   }
+  callWithVideo12(video) {
+    const { startCall12 } = this.props;
+    const config = { audio: true, video: false};
+    return () => startCall12(true, friendID, config);
+  }
+
   
+  render() {
+    const { clientId } = this.props;
+    console.log(`${clientId}`);
+    document.title = `${clientId} - VideoCall`;
+    return (
+      <div className="container main-window">
+        <div>
+          <h3>
+            Hi, your ID is
+            <input
+              type="text"
+              className="txt-clientId"
+              defaultValue={clientId}
+              readOnly
+            />
+          </h3>
+          <h4>Get started by calling a friend below</h4>
+        </div>
+        <div>
+          
+
+
+          <input
+            type="text"
+            className="txt-clientId"
+            spellCheck={false}
+            placeholder="Your friend ID"
+           onChange={event => friendID = event.target.value}
+          />
+          
+          <div>
+      
+            <button
+              type="button"
+              className="btn-action fa fa-video-camera"
+              onload={this.callWithVideo(true)}
+            />
+            <button
+              type="button"
+              className="btn-action fa fa-phone"
+              onload={this.callWithVideo12(false)}
+            />
+                
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 MainWindow.propTypes = {
   clientId: PropTypes.string.isRequired,
