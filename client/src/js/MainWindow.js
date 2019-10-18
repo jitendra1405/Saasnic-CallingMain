20dbc9303ec592c2129bed53d9558639ee676ff9
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-18 13:04:07 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -46,7 +46,7 @@ return (
          <button type="button" className="btn-action fa fa-phone"  onClick={this.callWithVideo12(false)}  />
          
 		 
-		 <a href="javascript:window.open('https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html/?varName={bcd}&fid={abc}','mywindowtitle','top=250,left=950,width=400,height=400,toolbar=no,scrollbars=no,location=no,resizable =no')" class="btn-action fa fa-phone"></a>
+		 <a href="javascript:window.open('https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html/?varName=bcd&fid=abc','mywindowtitle','top=250,left=950,width=400,height=400,toolbar=no,scrollbars=no,location=no,resizable =no')" class="btn-action fa fa-phone"></a>
       </div>
    </div>
 </div>
