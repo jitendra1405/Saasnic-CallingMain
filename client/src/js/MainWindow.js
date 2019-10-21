33b254c13e45e5686508c199334b018c124ef145
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-21 12:25:34 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -7,6 +7,8 @@ let searchParams = new URLSearchParams(url.search);
 var bcd = searchParams.get('varName');
 var abc = searchParams.get('FriendId');
 
+document.getElementById("data").innerHTML = bcd;
+
    
 
 
@@ -44,13 +46,13 @@ return (
       <input type="text" className="txt-clientId" spellCheck={false} placeholder="Your friend ID" defaultValue={abc} readOnly/>
       
 	  
-	  <div>
+	  <div id="val">
 	  
 	  <h4 class="title">Get started by calling a friend below</h4>
 	  
          <button type="button"className="btn-action fa fa-video-camera" onClick={this.callWithVideo(true)} />
          <button type="button" className="btn-action fa fa-phone"  onClick={this.callWithVideo12(false)}  />
-    		 <a href="javascript:window.open("https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html?abc=",'mywindowtitle','top=250,left=950,width=400,height=400')" class="btn-action fa fa-commenting-o"></a>
+    		 <a href="javascript:window.open('https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html?abc=','mywindowtitle','top=250,left=950,width=400,height=400')" class="btn-action fa fa-commenting-o"></a>
         </div>
    </div>
 </div>
