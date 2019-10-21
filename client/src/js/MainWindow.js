63e3f34ebcf283b08a06fb7827eb4ed8bad32f81
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-10-21 10:14:17 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -50,7 +50,9 @@ return (
          <button type="button"className="btn-action fa fa-video-camera" onClick={this.callWithVideo(true)} />
          <button type="button" className="btn-action fa fa-phone"  onClick={this.callWithVideo12(false)}  />
     		 <a href="javascript:window.open('https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html','mywindowtitle','top=250,left=950,width=400,height=400')" class="btn-action fa fa-commenting-o"></a>
-     
+         $.post("https://saasnicwebrtc.herokuapp.com/login", { abc : abc}, (data, status) => {
+          
+        });
 	 
          
      
