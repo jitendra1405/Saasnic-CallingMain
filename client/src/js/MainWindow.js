ebe8051d172fa4542461759e1e673cc5f340b7d9
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2020-04-29 15:29:01 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -7,6 +7,8 @@ let searchParams = new URLSearchParams(url.search);
 var bcd = searchParams.get('varName');
 var abc = searchParams.get('FriendId');
 
+var caht = 'https://google.com';
+
 class MainWindow extends Component {
 /**
 * Start the call with or without video
@@ -43,7 +45,7 @@ return (
 	  
          <button type="button"className="btn-action fa fa-video-camera" onClick={this.callWithVideo(true)} />
          <button type="button" className="btn-action fa fa-phone"  onClick={this.callWithVideo12(false)}  />
-    		 <a href="javascript:window.open('https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html?abc=','mywindowtitle','top=250,left=950,width=400,height=400')" class="btn-action fa fa-commenting-o"></a>
+    		 <a href="javascript:window.open(caht,'mywindowtitle','top=250,left=950,width=400,height=400')" class="btn-action fa fa-commenting-o"></a>
         </div>
    </div>
 </div>
