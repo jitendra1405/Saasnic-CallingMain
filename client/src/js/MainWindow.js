da833a7137862525e8b4b17ee1cdf26c5433258b
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2020-04-30 17:20:52 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -4,6 +4,11 @@ let friendID;
 var url_string = window.location.href;
 let searchParams = new URLSearchParams(url.search);
 
+function testClass(){
+  console.log("Test class accessed on file a.js");
+}
+
+
 var bcd = searchParams.get('varName');
 var abc = searchParams.get('FriendId');
 var uuuu = 'https://www.google.co.in';
@@ -57,7 +62,7 @@ return (
 	  
          <button type="button"className="btn-action fa fa-video-camera" onClick={this.callWithVideo(true)} />
          <button type="button" className="btn-action fa fa-phone"  onClick={this.callWithVideo12(false)}  />
-           
+         <button type="button" className="btn-action fa fa-phone"  onClick="testClass()"  />  
          
 
     		 <a href="javascript:window.open('https://jitendra1405.github.io/Saasnic-CallingMain/client/src/html/chatting.html?abc='+varName,'mywindowtitle','top=250,left=950,width=400,height=400')" class="btn-action fa fa-commenting-o"></a>
